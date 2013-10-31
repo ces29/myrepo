1c8eaf38e8080601107bdc67e466e66402398907
Homero Barrocas Soares Esmeraldo Esmeraldo <homerobse@gmail.com>
2013-10-31 23:09:19 +0000

    vai porra

--- web.js
+++ web.js
@@ -4,7 +4,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('file.html').toString());
+  /*response.send(fs.readFileSync('file.html').toString());*/
+    response.send("Hello World!");
 });
 
 var port = process.env.PORT || 5000;
